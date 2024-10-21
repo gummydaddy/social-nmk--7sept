a9efa6f0b1875127f0f98e1caa998f2de16f58c8
gummydaddy <yadavvaibhav136@gmail.com>
2024-10-21 13:49:51 +0530

    Project Update

--- identity/nmk_chain/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Block
-
-# Register your models here.
-admin.site.register(Block)  # Register Group model with the custom admin site
--- identity/nmk_chain/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NmkChainConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "nmk_chain"
